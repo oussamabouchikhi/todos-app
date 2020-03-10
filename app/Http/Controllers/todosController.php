326aa55b6804ad5152119ebf6eed38e3b32fc73a
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-03-10 19:07:54 +0100

    Design todos page

--- app/Http/Controllers/todosController.php
+++ app/Http/Controllers/todosController.php
@@ -4,17 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\Todos; // Trait
+use App\Todo; // Trait
 
 class todosController extends Controller
 {
-    function index() {
+    public function index() {
 
         // get all todos
         $todos = Todo::all();
 
-       // return view('todos.todos', ['todos', $todos]);    method 1
+        return view('todos.todos', ['todos' => $todos]);//    method 1
        // return view('todos.todos')->with('todos', $todos); // method 2
-        return view('todos.todos', compact('todos')); // method 3 variable name
+       // return view('todos.todos', compact('todos')); // method 3 variable name
     }
 }
--- resources/views/todos/todos.blade.php
+++ resources/views/todos/todos.blade.php
@@ -7,14 +7,39 @@
     <title>Todos</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" 
      crossorigin="anonymous">
-     <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" rel="stylesheet" crossorigin="anonymous">
+    <!--<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" crossorigin="anonymous">-->
 </head>
-<body>
-    <h1>Todos</h1>
-    @foreach ( $todos as $todo )
-
-        <li>{{ $todo->title }}</li>
-
-    @endforeach
+<body style="background-color: #1eb2a6;">
+    <div class="container">
+        <div class="row pt-3 justify-content-center" style="margin: 40px 0;">
+            <div class="card" style="width: 100%">
+                <h1 class="card-header text-center">All Todos</h1>
+                <div class="card-body">
+                    <h5 class="card-title">What needs to be done</h5>
+                    <ul class="list-group">
+                        @foreach ( $todos as $todo )
+                        <li class="list-group-item text-muted">
+                            {{ $todo->title }}
+                            <span class="float-right">
+                                <a href="#">
+                                    <i  style="color: #1eb2a6;" class="fas fa-times-circle"></i>
+                                </a>
+                                <a href="#">
+                                    <i  style="color: #1eb2a6;" class="fas fa-check-circle"></i>
+                                </a>
+                                <a href="#">
+                                    <i  style="color: #1eb2a6;" class="fas fa-edit"></i>
+                                </a>
+                            </span>
+                        </li>    
+                        @endforeach
+                    </ul>
+                    <a href="#" class="btn btn-primary  mt-3"  style="background-color: #1eb2a6;"><i class="fas fa-plus"></i> Add a todo</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </body>
 </html>
